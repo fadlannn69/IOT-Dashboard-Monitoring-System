e9f382d062e837a414476a3b83b68e4377c06699
fadlannn69 <rivaldifadlan1@gmail.com>
2026-02-05 09:37:41 +0700

    MEMULAI PROJECT UJIKOM

---
+++ ESP/ESP.ino
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
+#include "secrets.h"
+
+
+FirebaseData fbdo;
+FirebaseAuth auth;    
+FirebaseConfig config;
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // ===== WIFI =====
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nWiFi connected");
+
+  // ===== FIREBASE =====
+  config.api_key = " ";          
+  config.database_url = "https://ujikom-fadlann-default-rtdb.asia-southeast1.firebasedatabase.app";
+
+  Firebase.begin(&config, &auth);    
+  Firebase.reconnectNetwork(true);
+
+  Serial.println("Firebase connected");
+}
+void loop() {
+
+}
---
+++ ESP/secrets.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// ---------- WIFI ----------
+#define WIFI_SSID     "LABKOM1B"
+#define WIFI_PASSWORD "PPLGBISA01"
+
+// ---------- FIREBASE ----------
+#define REFERENCE_URL "https://ujikom-fadlann-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+#endif
