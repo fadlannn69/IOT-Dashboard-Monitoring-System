526123cc9bdcd46066ebae14f2491b2f6ed2d529
fadlannn69 <rivaldifadlan1@gmail.com>
2026-02-05 09:56:25 +0700

    Menambahkan Code Frontend Web

---
+++ Web/Config/Fire_Auth.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASYEAnSMK81XvAJ9rh5CtsPzd3RP0W8V8",
+  authDomain: "ujikom-fadlann.firebaseapp.com",
+  projectId: "ujikom-fadlann",
+  appId: "1:796931011810:web:8cd3cf1d86c60f902e5abb",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// ⬇️ INI URUTAN BENAR
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// ADMIN UID
+export const ADMIN_UID = "03ubYX7JlSThzpou2CP3jhVR0NA2";
