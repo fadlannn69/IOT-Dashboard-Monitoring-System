c2cdb7b4c8e4f3e54eb6e8d4623c8544ad3d3a65
Rivaldi-Fadlan <rivaldifadlan1@gmail.com>
2026-04-03 08:14:55 +0700

    Update Fire_Auth.js

--- docs/Web/Config/Fire_Auth.js
+++ docs/Web/Config/Fire_Auth.js
@@ -1,7 +1,10 @@
-// firebase.js
+////////// Fire_Auth.js //////////
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
+// CONFIG FIREBASE
 const firebaseConfig = {
   apiKey: "AIzaSyASYEAnSMK81XvAJ9rh5CtsPzd3RP0W8V8",
   authDomain: "ujikom-fadlann.firebaseapp.com",
@@ -9,9 +12,10 @@ const firebaseConfig = {
   appId: "1:796931011810:web:8cd3cf1d86c60f902e5abb",
 };
 
+// INIT APP
 const app = initializeApp(firebaseConfig);
 
+// EXPORT
 export const auth = getAuth(app);
-
-// ADMIN UID
-export const ADMIN_UID = "03ubYX7JlSThzpou2CP3jhVR0NA2";
+export const db = getDatabase(app); // Realtime Database
+export const ADMIN_UID = "03ubYX7JlSThzpou2CP3jhVR0NA2"; // UID admin
