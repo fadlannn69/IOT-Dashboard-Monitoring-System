5742a3fedf3d37777839dbe68f05c41e1ee74186
Rivaldi-Fadlan <rivaldifadlan1@gmail.com>
2026-04-03 08:44:06 +0700

    Update Login.js

--- docs/Login.js
+++ docs/Login.js
@@ -1,21 +1,16 @@
-////////// Login.js //////////
-
-import { auth, ADMIN_UID } from "./Web/Config/Fire_Auth.js";
+// login.js
+import { auth, ADMIN_UID } from "./Fire_Auth.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
-const msg = document.getElementById("msg");
 const btn = document.getElementById("login");
+const msg = document.getElementById("msg");
 
 btn.addEventListener("click", async () => {
-  msg.textContent = "Checking...";
-  btn.disabled = true;
-
   const email = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value;
 
   if (!email || !password) {
     msg.textContent = "Field kosong";
-    btn.disabled = false;
     return;
   }
 
@@ -23,17 +18,15 @@ btn.addEventListener("click", async () => {
     const cred = await signInWithEmailAndPassword(auth, email, password);
 
     if (cred.user.uid !== ADMIN_UID) {
-      await auth.signOut();
       msg.textContent = "Akses ditolak";
-      btn.disabled = false;
+      await auth.signOut();
       return;
     }
 
-    window.location.replace("./Web/Dashboard/Ujikom.html");
-
+    msg.textContent = "Login berhasil!";
+    window.location.replace("./Ujikom.html"); // buka dashboard
   } catch (err) {
     console.error(err);
     msg.textContent = "Email / Password salah";
-    btn.disabled = false;
   }
 });
